382adfa8cf6e6ceafb90fcfea12740b120e4bf98
simboroyacouba <80728664+simboroyacouba@users.noreply.github.com>
2024-07-25 17:03:41 +0100

    Update tasks.py

--- backend/api/views/tasks.py
+++ backend/api/views/tasks.py
@@ -5,24 +5,24 @@ from api.database.models import Task
 
 router = APIRouter()
 
-@router.get("/tasks", response_model=list[TaskSchema])
+@router.get("/", response_model=list[TaskSchema])
 async def get_tasks(db:Session=Depends(get_db)):
     tasks = Task.getAll(db)
     return tasks
 
-@router.post("/tasks", response_model=TaskSchema)
+@router.post("/", response_model=TaskSchema)
 async def create_task(task:TaskCreateSchema, db:Session=Depends(get_db)):
     task = Task(
         **task.__dict__
     ).saveWithCommit(db)
     return task.__dict__
 
-@router.get("/tasks/{id}", response_model=TaskSchema)
+@router.get("//{id}", response_model=TaskSchema)
 async def get_task(id:int, db:Session=Depends(get_db)):
     task = Task.get(db, id=id)
     return task
     
-@router.put("/tasks/{id}", response_model=TaskSchema)
+@router.put("//{id}", response_model=TaskSchema)
 async def update_task(id:int, data:TaskUpdateSchema, db:Session=Depends(get_db)):
     task = Task.get(db, id=id)
 
